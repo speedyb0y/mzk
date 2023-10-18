209672e751d069a3940b8cd55ec4f41431e9e882
Willian B <speedyb0y@users.noreply.github.com>
2023-10-18 13:04:25 -0300

    ss

--- deadbeef/fs.c
+++ deadbeef/fs.c
@@ -64,7 +64,7 @@ static int do_opendir (const char* fpath, struct fuse_file_info* fi) {
     return 0;
 }
 
-static int do_readdir (const char* fpath, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
+static int do_readdir (const char* fpath, void *buffer, fuse_fill_dir_t filler, off_t offset, fuse_file_info_s* finfo) {
 
     const size_t sid = finfo ? finfo->fh : strcmp(fpath, "/") ? songs_lookup(db->songsTree, fpath_code(fpath)) : SONGS_N;
 
