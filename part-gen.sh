fd36d0668da2ca618f6a1724b6cf50ba6a8536ce
Willian B <speedyb0y@users.noreply.github.com>
2023-10-23 16:29:53 -0300

    Update part-gen.sh

--- part-gen.sh
+++ part-gen.sh
@@ -11,16 +11,13 @@
     cd /MZK
     
     for DEVICE in /dev/sd[a-z]{[0-9],[0-9][0-9],[0-9][0-9][0-9]} ; do
-    	if [ -e ${DEVICE} ] ; then
-    		LABEL=$(dd status=none if=${DEVICE} skip=32808 iflag=skip_bytes bs=64 count=1 | grep --only-matching --text -E '^[0-9A-Za-z-]{1,28}-[0-9A-Za-z-]{4,32}')
-    		case ${LABEL} in
-    			MZK-*)
-    				FOLDER=${LABEL/MZK-/.}
-    				echo mkdir -p ${FOLDER}
-    				echo mount ${DEVICE} ${FOLDER} -t iso9660 -o ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,dmode=0555,mode=0444,map=off,block=2048
-                    echo ln -sfn ${FOLDER}/XXX .
-    			;;
-    		esac
+    	if [ -b ${DEVICE} ] ; then
+    		if LABEL=$(dd status=none if=${DEVICE} skip=32808 iflag=skip_bytes bs=32 count=1 | grep --text -E '^MZK-[0-9A-Za-z]{8,32}$') ; then
+                FOLDER=${LABEL/MZK-/.}
+                mkdir -p ${FOLDER}
+                mount ${DEVICE} ${FOLDER} -t iso9660 -o ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,dmode=0555,mode=0444,map=off,block=2048
+                ln -sfn ${FOLDER}/* .
+            fi
     	fi
     done
 )
