bbb617cbb6c83c0311370da9c75ee25c2847c1b4
Willian B <speedyb0y@users.noreply.github.com>
2023-10-23 16:42:51 -0300

    Update part-gen.sh

--- part-gen.sh
+++ part-gen.sh
@@ -12,8 +12,9 @@
         if [ -b ${DEVICE} ] ; then
             if LABEL=$(dd status=none if=${DEVICE} skip=32808 iflag=skip_bytes bs=32 count=1 | grep --text -E '^MZK-[0-9A-Za-z]{8,32}$') ; then
                 FOLDER=${LABEL/MZK-/.}
-                mkdir -p ${FOLDER}
-                mount ${DEVICE} ${FOLDER} -t iso9660 -o ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,dmode=0555,mode=0444,map=off,block=2048
+                mkdir      ${FOLDER}
+                chmod 0111 ${FOLDER}
+                mount ${DEVICE} ${FOLDER} -t iso9660 -o ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,map=off,block=2048,dmode=0111,mode=0444
                 ln -sfn ${FOLDER}/* .
             fi
         fi
