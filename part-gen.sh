3d00392907fb06d440e4e433c4fcefda7d9e912c
Willian B <speedyb0y@users.noreply.github.com>
2023-10-23 16:22:06 -0300

    Update part-gen.sh

--- part-gen.sh
+++ part-gen.sh
@@ -4,15 +4,23 @@
 #
 
 #
-for DEVICE in /dev/sd[a-z]{[0-9],[0-9][0-9],[0-9][0-9][0-9]} ; do
-	if [ -e ${DEVICE} ] ; then
-		LABEL=$(dd status=none if=${DEVICE} skip=32808 iflag=skip_bytes bs=64 count=1 | grep --only-matching --text -E '^[0-9A-Za-z-]{1,28}-[0-9A-Za-z-]{4,32}')
-		case ${LABEL} in
-			*-*)
-				FOLDER=/mnt/${LABEL/-//}
-				mkdir -p ${FOLDER}
-				echo ${DEVICE} ${FOLDER} iso9660 noauto,ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,mode=0444,map=off,block=2048 0 0
-			;;
-		esac
-	fi
-done
+( set -e
+
+    mount /MZK /MZK -t tmpfs -o rw,noexec,nodev,nosuid
+
+    cd /MZK
+    
+    for DEVICE in /dev/sd[a-z]{[0-9],[0-9][0-9],[0-9][0-9][0-9]} ; do
+    	if [ -e ${DEVICE} ] ; then
+    		LABEL=$(dd status=none if=${DEVICE} skip=32808 iflag=skip_bytes bs=64 count=1 | grep --only-matching --text -E '^[0-9A-Za-z-]{1,28}-[0-9A-Za-z-]{4,32}')
+    		case ${LABEL} in
+    			MZK-*)
+    				FOLDER=${LABEL/MZK-/.}
+    				echo mkdir -p ${FOLDER}
+    				echo mount ${DEVICE} ${FOLDER} -t iso9660 -o ro,noexec,nodev,norock,nojoliet,check=strict,uid=0,gid=0,dmode=0555,mode=0444,map=off,block=2048
+                    echo ln -sfn ${FOLDER}/XXX .
+    			;;
+    		esac
+    	fi
+    done
+)
