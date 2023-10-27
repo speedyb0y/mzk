eccce72644de6e913e9328ba0a0da490d546f94a
Willian B <speedyb0y@users.noreply.github.com>
2023-10-27 12:28:19 -0300

    sss

--- mkiso2.py
+++ mkiso2.py
@@ -154,7 +154,7 @@ end = 0
 # ACHA O NOSSO READER
 while (h := omap.find(b'ISOFS64\x00', 0, end)) == -1:
     #c = pipeIO.readinto(oview[end:end+4*1024*1024])
-    chunk = pipe.read(4*1024*1024).encode()
+    chunk = os.read(pipe.fileno(), 4*1024*1024)
     c = len(chunk)
     oview[end:end+c] = chunk
     print(f'@ {end} leu {c}')
