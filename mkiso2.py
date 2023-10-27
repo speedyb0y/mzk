91e29f057a8a162934a570ba2464bcab3b4f56f8
Willian B <speedyb0y@users.noreply.github.com>
2023-10-27 12:31:02 -0300

    ss

--- mkiso2.py
+++ mkiso2.py
@@ -153,13 +153,13 @@ end = 0
 
 # ACHA O NOSSO READER
 while (h := omap.find(b'ISOFS64\x00', 0, end)) == -1:
-    #c = pipeIO.readinto(oview[end:end+4*1024*1024])
-    chunk = os.read(pipe.fileno(), 4*1024*1024)
-    c = len(chunk)
+    c = pipeIO.readinto(oview[end:end+4*1024*1024])
+    #chunk = os.read(pipe.fileno(), 4*1024*1024)
+    #c = len(chunk)
     print(f'@ {end} leu {c}', len(omap), len(oview), end+c)
     if c == 0:
         break
-    oview[end:end+c] = chunk
+    #oview[end:end+c] = chunk
     end += c
 
 print('HEADER AT:', h, repr(omap[:128]), end)
