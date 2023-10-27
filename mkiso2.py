135cfa265b3d5aa34c958334a3d765ea7a073c38
Willian B <speedyb0y@users.noreply.github.com>
2023-10-27 11:58:38 -0300

    ddd

--- mkiso2.py
+++ mkiso2.py
@@ -154,6 +154,7 @@ end = 0
 # ACHA O NOSSO READER
 while (h := omap.find(b'ISOFS64\x00', 0, end)) == -1:
     c = pipeIO.readinto(oview[end:end+4*1024*1024])
+    print('leu:', c)
     if c == 0:
         break
     end += c
