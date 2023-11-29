92c4af9b931fa28ed589576f8319b13a21d3978a
Willian B <speedyb0y@users.noreply.github.com>
2023-11-29 03:35:01 -0300

    Update acoustid.py

--- acoustid.py
+++ acoustid.py
@@ -38,3 +38,29 @@ for f in sys.argv[1:]:
             continue
 
         print((xid, duration, fingerprint, response))
+
+'''
+mkdir FINGERPRINTS ERRORS
+
+blockdev --setra 2048 /dev/sdd
+
+DIRS_0="CONVERTED sdd1 sdd3/CONVERTED"
+DIRS_1="sdd1 CONVERTED sdd3/CONVERTED"
+DIRS_2="sdd3/CONVERTED sdd1 CONVERTED"
+
+find ${DIRS_0} -type f -iname '[ABC@1]*' -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/0  2> ERRORS/0  &
+find ${DIRS_1} -type f -iname '[DEF9]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/1  2> ERRORS/1  &
+find ${DIRS_2} -type f -iname '[GHI8]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/2  2> ERRORS/2  &
+find ${DIRS_0} -type f -iname '[JKL7]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/3  2> ERRORS/3  &
+find ${DIRS_1} -type f -iname '[MNO6]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/4  2> ERRORS/4  &
+find ${DIRS_2} -type f -iname '[PQR5]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/5  2> ERRORS/5  &
+find ${DIRS_0} -type f -iname '[STU4]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/6  2> ERRORS/6  &
+find ${DIRS_1} -type f -iname '[VXZ3]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/7  2> ERRORS/7  &
+find ${DIRS_2} -type f -iname '[WY02]*'  -printf '\0%p ' -exec fpcalc  '{}' \; > FINGERPRINTS/8  2> ERRORS/8  &
+
+fg ; fg ; fg ; fg ; fg ; fg ; fg ; fg ; fg ; fg ; fg ; fg
+
+wait ; wait ; wait ; wait ; wait ; wait ; wait ; wait ; wait ; wait
+
+cat FINGERPRINTS/* | tr  '\n' ' ' | tr '\0' '\n' | awk -F / '{print $NF}' | sort -n > FINGERPRINTS.txt
+'''
