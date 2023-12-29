a3c5717c963f6c7a5f4a239553441e6d22cdb9f2
speedyb0y <speedyb0y.rev0ltz@gmail.com>
2023-12-28 22:45:56 -0300

    1703814356

--- cbor-cached.py
+++ cbor-cached.py
@@ -2,25 +2,91 @@
 
 import cbor2 as cbor
 
-CACHE_VALUES = []
-CACHE_CODES = {}
+#
+class CData:
 
-def V (obj):
-    if isinstance(obj, (tuple, list, set)):
-        code = type(obj)(map(V, obj))
-    elif isinstance(obj, dict):
-        code = { V(k): V(v) for k, v in obj.items()}
-    elif obj is None or obj is False or obj is True:
-        code = obj
-    else:
-        assert obj is None or isinstance(obj, (bool, int, float, str, bytes))
-        try:
-            code, counter = CACHE_CODES[obj]
-        except KeyError:
-            CACHE_VALUES.append(obj)
-            code, counter = CACHE_CODES[obj] = len(CACHE_CODES), iter(range(0xFFFFFFFF))
-        next(counter)
-    return code
+    def __init__ (self, x=None):
+        self.CODES = {}
+        self.VALS = []
+        if x is not None:
+            x = self.ENC(x)
+        self.x = x
+
+    def append (self, v):
+        self.x.append(self.ENC(v))
+
+    def add (self, v):
+        self.x.add(self.ENC(v))
+
+    # CACHE A VALUE, AND ENCODE IT AS CODE
+    def ENC (self, obj):
+        if isinstance(obj, (tuple, list, set)):
+            code = type(obj)(map(self.ENC, obj))
+        elif isinstance(obj, dict):
+            code = { self.ENC(k): self.ENC(v) for k, v in obj.items()}
+        elif (obj is None
+           or obj is False
+           or obj is True):
+            code = obj
+        else:
+            assert obj is None or isinstance(obj, (bool, int, float, str, bytes))
+            try:
+                code, counter = self.CODES[obj]
+            except KeyError:
+                self.VALS.append(obj)
+                code, counter = self.CODES[obj] = len(self.CODES), iter(range(0xFFFFFFFF))
+            next(counter)
+        return code
+
+    # OPTIMIZE
+    def OPTIMIZE (self, code):
+        if code is None or code is False or code is True:
+            code2 = code
+        elif isinstance(code, int):
+            code2 = self.NOVOS[code]
+        elif isinstance(code, (tuple, list, set)):
+            code2 = type(code)(map(self.OPTIMIZE, code))
+        else:
+            code2 = { self.OPTIMIZE(k): self.OPTIMIZE(v) for k, v in code.items()}
+        return code2
+
+    # STORE
+    def encode (self):
+        assert len(self.CODES) == len(self.VALS)
+        # OTIMIZA
+        # CODIGOS, ORDENADOS PELA QUANTIDADE DE USOS
+        NOVOS = [old for _, old in sorted(((next(counter), code) for obj, (code, counter) in self.CODES.items()), reverse=True)]
+        self.VALS = [ self.VALS[code] for code in NOVOS ]
+      # self.NOVOS = { old: i for i, old in enumerate(NOVOS) }
+        self.NOVOS = [ b for a, b in sorted((old, i) for i, old in enumerate(NOVOS)) ]
+        encoded = cbor.dumps((self.VALS, self.OPTIMIZE(self.x)))
+        self.CODES   = None
+        self.VALS    = None
+        self.NOVOS   = None
+        self.x = None
+        return encoded
+
+# LOAD
+def CDATA_LOAD (encoded):
+
+    # DECODE A CODE, TO ITS VALUE
+    def DEC (code):
+        if code is None or code is False or code is True:
+            obj = code
+        elif isinstance(code, int):
+            obj = VALS[code]
+        elif isinstance(code, (tuple, list, set)):
+            obj = type(code)(map(DEC, code))
+        else:
+            obj = { DEC(k): DEC(v) for k, v in code.items() }
+        return obj
+
+    VALS, x = cbor.loads(encoded)
+    
+    return DEC(x)
+
+
+######
 
 orig = ( 1, 2,
     # 'b', 'banana', 'c', 'laranja', 324234, 'banana',
@@ -34,42 +100,9 @@ orig = ( 1, 2,
     'quatro', 1, 0.2, -0.3
     )
 
-encoded = V(orig)
+encoded = CData(orig).encode()
 
-# OTIMIZA
-# CODIGOS, ORDENADOS PELA QUANTIDADE DE USOS
-NOVOS = [old for _, old in sorted(((next(counter), code) for obj, (code, counter) in CACHE_CODES.items()), reverse=True)]
-CACHE_VALUES = [ CACHE_VALUES[code] for code in NOVOS ]
-# NOVOS = { old: i for i, old in enumerate(NOVOS) }
-NOVOS = [ b for a, b in sorted((old, i) for i, old in enumerate(NOVOS)) ]
-
-def OPTIMIZE (code):    
-    if code is None or code is False or code is True:
-        code2 = code
-    elif isinstance(code, int):
-        code2 = NOVOS[code]
-    elif isinstance(code, (tuple, list, set)):
-        code2 = type(code)(map(OPTIMIZE, code))
-    else:
-        code2 = { OPTIMIZE(k): OPTIMIZE(v) for k, v in code.items()}
-    return code2
-
-encoded = cbor.dumps((CACHE_VALUES, OPTIMIZE(encoded)))
-
-# RESTAURA
-def RESTAURA (code):
-    if code is None or code is False or code is True:
-        obj = code
-    elif isinstance(code, int):
-        obj = CACHE_VALUES[code]
-    elif isinstance(code, (tuple, list, set)):
-        obj = type(code)(map(RESTAURA, code))
-    else:
-        obj = { RESTAURA(k): RESTAURA(v) for k, v in code.items()}
-    return obj
-
-CACHE_VALUES, decoded = cbor.loads(encoded)
-decoded = RESTAURA(decoded)
+decoded = CDATA_LOAD(encoded)
 
 print('ORIGINAL:', orig)
 print('ENCODED-CBOR:', len(encoded)/len(cbor.dumps(orig)))
